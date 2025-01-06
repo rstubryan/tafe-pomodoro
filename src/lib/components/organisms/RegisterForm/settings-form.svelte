681bb5c1443b51b796fdc8ecee41ba211cc3a6b3
rstubryan <restubumirr@gmail.com>
2025-01-06 08:26:06 +0700

    feat(auth): add register form component and route logic

---
+++ src/lib/components/organisms/RegisterForm/settings-form.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	import * as Form from '$lib/components/ui/form';
+	import * as Card from '$lib/components/ui/card';
+	import { Input } from '$lib/components/ui/input';
+	import { formSchema, type FormSchema } from '$/routes/(auth)/register/schema';
+	import { type SuperValidated, type Infer, superForm } from 'sveltekit-superforms';
+	import { zodClient } from 'sveltekit-superforms/adapters';
+
+	export let data: SuperValidated<Infer<FormSchema>>;
+
+	const form = superForm(data, {
+		validators: zodClient(formSchema)
+	});
+
+	const { form: formData, enhance } = form;
+</script>
+
+<Card.Root>
+	<Card.Header>
+		<Card.Title class="text-center text-2xl font-medium">Register</Card.Title>
+	</Card.Header>
+	<form method="POST" use:enhance>
+		<Card.Content>
+			<Form.Field {form} name="username">
+				<Form.Control let:attrs>
+					<Form.Label for="username">Username</Form.Label>
+					<Input type="text" {...attrs} bind:value={$formData.username} />
+				</Form.Control>
+				<Form.FieldErrors />
+			</Form.Field>
+			<Form.Field {form} name="email">
+				<Form.Control let:attrs>
+					<Form.Label>Email</Form.Label>
+					<Input type="email" {...attrs} bind:value={$formData.email} />
+				</Form.Control>
+				<Form.FieldErrors />
+			</Form.Field>
+			<Form.Field {form} name="password">
+				<Form.Control let:attrs>
+					<Form.Label>Password</Form.Label>
+					<Input type="password" {...attrs} bind:value={$formData.password} />
+				</Form.Control>
+				<Form.FieldErrors />
+			</Form.Field>
+		</Card.Content>
+		<Card.Footer>
+			<Form.Button class="w-full">Submit</Form.Button>
+		</Card.Footer>
+	</form>
+</Card.Root>
---
+++ src/routes/(auth)/register/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import type { PageData } from './$types.js';
+	import SettingsForm from '$lib/components/organisms/RegisterForm/settings-form.svelte';
+	export let data: PageData;
+</script>
+
+<SettingsForm data={data.form} />
