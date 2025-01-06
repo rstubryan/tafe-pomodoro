8a178e9692574fa0cc10c71ae0177401107f526e
Restu <96672455+rstubryan@users.noreply.github.com>
2025-01-06 08:21:38 +0700

    Deps/library update (#1)
    * chore(deps): update package-lock.json & regenerate package.json
    
    * chore(deps): update prettier to v3.4.2
    
    * chore(eslint): update eslint config and add prettier config
    
    * chore(deps): add bits-ui, clsx, tailwind-merge, and tailwind-variants
    
    * chore: initialize shadcn-ui components and utils setup
    
    * feat(button): add button component and styles
    
    * chore(svelte): update alias to $lib
    
    * feat(card): add card component and sub-components
    
    * chore(svelte): add misc.xml for vite config
    
    * chore(deps): add formsnap, sveltekit-superforms, and zod
    
    * chore(deps): add axios and tanstack query client
    
    * chore(deps): add js-cookie and jwt-decode
    
    * chore(eslint): update eslint config
    
    * chore(routes): remove login and register routes
    
    * feat(form): add form components

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,8 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
 		alias: {
-			$: path.resolve('src')
+			$: path.resolve('src'),
+			$lib: path.resolve('src/lib')
 		}
 	}
 };
